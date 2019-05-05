5d30258a1af875f9b8fa320a0b31978448608856
Bhavna Vadlamani <bhavna.vadlamani94@gmail.com>
2019-05-05 22:01:41 +0530

    Changes

---
+++ BindableItemSourceList/BindableItemSourceList/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BindableItemSourceList
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<TextAndDetail> _dataList;
+
+        public ObservableCollection<TextAndDetail> DataList
+        {
+            get => _dataList;
+            set => SetProperty(ref _dataList, value);
+        }
+
+        public MainViewModel()
+        {
+            var data = new List<TextAndDetail>
+            {
+                new TextAndDetail{Text = "India", Detail = "New Delhi"},
+                new TextAndDetail { Text = "Ireland", Detail = "Dublin" },
+                new TextAndDetail { Text = "Italy", Detail = "Rome" }
+            };
+            DataList = new ObservableCollection<TextAndDetail>(data);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ BindableItemSourceList/BindableItemSourceList/TitleAndValueCellList.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BindableItemSourceList
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TitleAndValueCellList : ContentView
+	{
+        public static readonly BindableProperty ItemsSourceProperty =
+            BindableProperty.Create(
+                nameof(ItemsSource), typeof(IEnumerable), typeof(TitleAndValueCellList));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public TitleAndValueCellList ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
