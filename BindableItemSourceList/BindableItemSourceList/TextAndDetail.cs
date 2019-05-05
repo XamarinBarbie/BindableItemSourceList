13a29a55d9140912acb3f90bd962a5df88c4f666
XamarinBarbie <47385538+XamarinBarbie@users.noreply.github.com>
2019-05-05 23:04:33 +0530

    Changes (#1)

---
+++ BindableItemSourceList/BindableItemSourceList/TextAndDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BindableItemSourceList
+{
+    class TextAndDetail
+    {
+        public string Text { get; set; }
+        public string Detail { get; set; }
+    }
+}
